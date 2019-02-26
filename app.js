a4ddef4aff37aa2bfe6aef268d6f5e745c0317df
Zeeraa <udenanton@hotmail.com>
2019-02-26 21:20:56 +0100

    Added price in SEK

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const request = require('request');
-const Discord = require('discord.js');
-const config  = require('./config.json');
-const client  = new Discord.Client();
+const xml2json     = require('xml2json');
+const request      = require('request');
+const sync_request = require('sync-request');
+const Discord      = require('discord.js');
+const config       = require('./config.json');
+const client       = new Discord.Client();
 
 client.on('ready', () => {
 	console.log('Discord client ready!');
@@ -18,7 +20,7 @@ client.on('message', message => {
 			let msg = message.content.toLowerCase();
 			let args = msg.split(" ");
 			args.splice(0, 1);
-			if(msg.startsWith('!info')) {
+			if(msg.toLowerCase().startsWith('!info ') || msg.toLowerCase() == '!info') {
 				if(args.length == 1) {
 					console.log('Checking info for ' + args[0]);
 					request(config.api_url + '/cardinfo/' + args[0], function (error, response, body) {
@@ -63,7 +65,7 @@ client.on('message', message => {
 				} else {
 					message.reply('Useage: !info <PrintTag>');
 				}
-			} else if(msg.startsWith('!price')) {
+			} else if(msg.toLowerCase().startsWith('!price ') || msg.toLowerCase() == '!price') {
 				if(args.length == 1) {
 					console.log('Checking price for ' + args[0]);
 					request(config.api_url + '/cardprice/' + args[0], function (error, response, body) {
@@ -85,18 +87,56 @@ client.on('message', message => {
 									result += '\nRarity: ' + data.data.card.price_data.rarity;
 									result += '\nCardmarket price: ' + data.data.cardmarket.lowest_price + ' €';
 
+									let priceSek = null;
+									console.log('checking price in SEK');
+									try {
+										let res = sync_request('GET', 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
+										let ratesJSON = JSON.parse(xml2json.toJson(res.getBody()));
+										let rates = ratesJSON['gesmes:Envelope'].Cube.Cube.Cube;
+										let rateSEK = null;
+
+										for(let i = 0; i < rates.length; i++) {
+											if(rates[i].currency == 'SEK') {
+												rateSEK = parseFloat(rates[i].rate);
+												break;
+											}
+										}
+
+										if(rateSEK == null) {
+											console.warn('Failed to get SEK rate');
+										} else {
+											console.log('SEK Rate: ' + rateSEK);
+											let priceSekOriginal = data.data.cardmarket.lowest_price * rateSEK;
+											priceSek = priceSekOriginal.toFixed(2);
+
+											console.log('Full price: ' + priceSekOriginal + ' SEK. Price: ' + priceSek + ' SEK');
+
+										}
+									} catch(err) {
+										console.log(err);
+									}
+
+									if(priceSek != null) {
+										result += '\nCardmarket price: ' + priceSek + ' SEK';
+									}
+
 									message.reply(result);
 								}
 							} else {
 								if(data.cardmarket_error) {
-									console.log('Failed to find card price for ' + args[0].toUpperCase() + '. data.message: ' + data.message);
+									console.log('Failed to find cardmarket price for ' + args[0].toUpperCase() + '. data.message: ' + data.message);
 									message.reply('Failed to extract cardmarket price!');
 								} else if(data.error) {
 									console.log('Failed to find card ' + args[0].toUpperCase() + '. data.message: ' + data.message);
 									message.reply('An error occurred! Please try again later');
 								} else {
-									console.log('Card with print tag ' + args[0].toUpperCase() + ' not found. data.message: ' + data.message);
-									message.reply('Could not find card with print tag \'' + args[0].toUpperCase() + '\'');
+									if(data.data == undefined) {
+										console.log('Card with print tag ' + args[0].toUpperCase() + ' not found. data.message: ' + data.message);
+										message.reply('Could not find card with print tag \'' + args[0].toUpperCase() + '\'');
+									} else {
+										console.log('Failed to find cardmarket price for ' + args[0].toUpperCase() + '. data.message: ' + data.message);
+										message.reply('Failed to find cardmarket price!');
+									}
 								}
 							}
 						} catch(err) {
@@ -107,7 +147,7 @@ client.on('message', message => {
 				} else {
 					message.reply("Useage: !price <PrintTag>");
 				}
-			} else if(msg.startsWith('!help')) {
+			} else if(msg.toLowerCase().startsWith('!help ') || msg.toLowerCase() == '!help') {
 				message.reply('\n!help\n!info <PrintTag>\n!price <PrintTag>');
 			} else {
 				message.reply('Unknown command. Use !help for a list of commands');
